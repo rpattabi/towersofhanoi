4240d3d20b60493706af5d86f1e492415560bd61
ragunathan.pattabiraman <ragunathan.pattabiraman@65a28242-a519-0410-bb55-7b42abb2b390>
2006-08-20 10:28:23 +0000

    Command Pattern update complete

--- trunk/Player.rb
+++ trunk/Player.rb
@@ -5,11 +5,12 @@ class Player
 	@towers
 	@disks
 	@direction
+	@diskMover
 	
 	def initialize( towers, disks )
 		@towers = towers
 		@disks	= disks
-		@diskMoverCmd = DiskMoverCmd.new
+		@diskMover = DiskMover.new()
 		
 		#@towers[SOURCE].display
 		displayTowers #refactor# Display is not the job of the player.
@@ -34,7 +35,7 @@ class Player
 		end
 
 		#@towers[DEST].display
-		
+		@diskMover.replay
 	end
 	
 	def move( disk ) #refactor# command pattern
@@ -54,7 +55,7 @@ class Player
 		
 		if tmpTower != disk.getTower
     		#move it to tmpTower
-    	    @diskMoverCmd.move disk, disk.getTower, tmptower
+    	  @diskMover.move(Move.new(disk, disk.getTower, tmpTower))
     		displayTowers #refactor#
 		end
 	end
@@ -68,7 +69,7 @@ class Player
 		
 		if tmpTower != disk.getTower
     		#move it to tmpTower
-    	    @diskMoverCmd.move disk, disk.getTower, tmptower
+    	  @diskMover.move(Move.new(disk, disk.getTower, tmpTower))
     		displayTowers #refactor#
 		end
 	end
@@ -85,7 +86,7 @@ class Player
 		return @towers[SOURCE]
 	end
 	
-	def rightTower(tower)
+	def rightTower(tower) Tower
 		if tower.source?
 			return @towers[INTERM]
 		end
@@ -97,49 +98,57 @@ class Player
 		return @towers[DEST]	
 	end
 	
-    def displayTowers()				
-      #puts '000000000000000000'	
-      @towers[SOURCE].display
-      puts "SOURCE"
-      puts "\n"*2
-      
-      @towers[INTERM].display
-      puts "INTERM"
-      puts "\n"*2
-      
-      @towers[DEST].display
-      puts "DEST"
-      #puts "\n"*3
-      puts '000000000000000000'
-      puts "\n"*4
-    end
+	def displayTowers()				
+		#puts '000000000000000000'	
+		@towers[SOURCE].display
+		puts "SOURCE"
+		puts "\n"*2
+		
+		@towers[INTERM].display
+		puts "INTERM"
+		puts "\n"*2
+		
+		@towers[DEST].display
+		puts "DEST"
+		#puts "\n"*3
+		puts '000000000000000000'
+		puts "\n"*4
+	end
 end
 
 
-class DiskMoverCmd
+class DiskMover #invoker or the Player is invoker?
   def initialize()
-    @moveLog = []
+    @moves = []
+		#@curMove = NoMove.new
   end
   
-  def move( disk, toTower )
+  def move( move ) # >
     # log the movement
-    @moveLog.push MoveStruct.new( disk, disk.getTower, toTower )
-    
-    # first remove the disk from the tower it is on
-    # then move it to the toTower
-    disk.getTower.MoveTop
-    toTower.AddDisk disk  
+    @moves.push move
+    move.execute
+
+		#@curMove = move
   end
+	
+	def unmove() # <<
+		#@moves.pop.undo
+		#@curMove = @moves[@moves.size-2]
+	end
+	
+	def redomove() # >>
+		#@curMove.execute
+	end
   
   def replay()
-    @moveLog.each do |move|
+    @moves.each do |move|
       move.printMove
     end
   end 
 end
 
 
-class Move
+class Move #command
   attr :disk
   attr :fromTower
   attr :toTower
@@ -149,10 +158,49 @@ class Move
     @fromTower  = fromTower
     @toTower    = toTower
   end
-  
+
+	def execute()
+    # first remove the disk from the tower it is on
+    @fromTower.MoveTop
+		
+		# move the disk to toTower
+    @toTower.AddDisk @disk  
+	end
+	
+	def undo()
+		@toTower.MoveTop
+		@fromTower.AddDisk @disk
+	end
+	
+	def log( file )
+		file.append "\n" + " Disk " + @disk.getSize.to_s +
+         " is moved from Tower " + @fromTower.designation.to_s +
+         " to Tower " + @toTower.designation.to_s
+	end
+	
   def printMove()
-    puts " Disk " + disk.size.to_s +
-         " is moved from Tower " + fromTower.designation
-         " to Tower " + toTower.designation
+    puts "\n" + " Disk " + @disk.getSize.to_s +
+         " is moved from Tower " + @fromTower.designation.to_s +
+         " to Tower " + @toTower.designation.to_s
+  end
+end
+
+class NoMove #null object
+  def initialize( disk, fromTower, toTower)
+  end
+	
+	def initialize()
+	end
+
+	def execute()
+	end
+	
+	def undo()
+	end
+	
+	def log( file )
+	end
+	
+  def printMove()
   end
 end
